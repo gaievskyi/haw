2f91c74b393f5b7e14fa9964dae2e608e5cac0cb
beeklz <beeklzwork@gmail.com>
2021-06-01 23:41:59 +0300

    Add temporary alert on bttns (should be a modal window then)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
 "use strict";
 
+const becomeFamilyBttns = document.querySelectorAll(
+  ".btn-support,.support__btn"
+);
+console.log(becomeFamilyBttns);
+
+const msg =
+  "Przekazanie 1% podatku, nr KRS 0000154454, w celu szczegółowym wpisując Schronisko w Mysłowicach\n" +
+  "lub przekazanie darowizny na nasz rachunek bankowy:\n" +
+  "39 1020 2313 0000 3102 0121 3677\n" +
+  "lub\n" +
+  "63 1020 2528 0000 0702 0498 9523\n" +
+  "Przekazanie darowizny w schronisku\n";
+
+[...becomeFamilyBttns].forEach((btn) => (btn.onclick = () => alert(msg)));
+
 window.addEventListener("load", () => {
   document.body.setAttribute("style", "overflow:hidden;");
   const loader = document.querySelector(".loader-wrapper");
